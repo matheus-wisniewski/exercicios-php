75d7d62831edaf0572453494e67b5bf6394248a9
Matheus W <maahwiski@gmail.com>
2024-07-24 01:26:56 -0300

    exercicios e desafios iniciais

--- desafios/desafio001/resposta.php
+++ desafios/desafio001/resposta.php
@@ -1,9 +1,3 @@
-<?php 
-    $inputNumber = $_POST["numero"];
-    $antecessor = $inputNumber - 1;
-    $sucessor = $inputNumber + 1;
-?>
-
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -18,49 +12,17 @@
         <h1>Resultado Final</h1>
 
         <?php 
+            $inputNumber = $_POST["numero"];
+            $antecessor = $inputNumber - 1;
+            $sucessor = $inputNumber + 1;
+            
             echo "<p>O número escolhido foi: $inputNumber</p>";
             echo "<p>O seu antecessor é: $antecessor</p>";
             echo "<p>O seu sucessor é: $sucessor</p>";
         ?>
 
-        <button id="btn-redirecionar">Voltar</button>
+        <button onclick="javascript:history.go(-1)">Voltar</button>
     </div>
 </body>
-<style>
-body {
-    background-color: #A4A9AD;
-    width: 100vw;
-    height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-}
-
-div {
-    align-items: center;
-    background-color: white;
-    border-radius: 5px;
-    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    height: 20rem;
-    transition: 0.3s;
-    width: 30rem;
-}
-
-div:hover {
-    box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
-}
-
-button {
-    width: 100px;
-}
-</style>
-<script>
-document.getElementById('btn-redirecionar').addEventListener('click', function() {
-    history.go(-1);
-});
-</script>
 
 </html>
